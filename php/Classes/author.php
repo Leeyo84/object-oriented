dc1c66720756871ba7e266bd9ea8c9c9077330d0
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-16 19:12:16 -0600

    added table to author.sql and started class in author.php

---
+++ php/Classes/author.php
@@ -0,0 +1,19 @@
+<?php
+namespace DeepDiveCoders\ObjectOrientation;
+
+require_once ("autoload.php");
+require_once (dirname(__DIR__)) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ *  h1 short description of what the class does.
+ *
+ * p more detailed description, a couple of sentences.
+ *
+ * @author Leonela Gutierrez <Leonele_Gutierrez@hotmail.com>
+ */
+class author {
+
+}
+?>
