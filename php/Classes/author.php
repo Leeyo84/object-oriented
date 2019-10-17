ffb8e6b95c03aa72d22f33c9faadba188604160a
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-16 19:51:48 -0600

    added state variables to author.php

--- php/Classes/author.php
+++ php/Classes/author.php
@@ -13,7 +13,47 @@ use Ramsey\Uuid\Uuid;
  *
  * @author Leonela Gutierrez <Leonele_Gutierrez@hotmail.com>
  */
-class author {
+class Author implements \JsonSerializable {
+	use ValidateDate;
+	use ValidateUuid;
+	/**
+	 * id for this Author; this is the primary key
+	 * @var Uuid $AuthorId
+	*/
+	private $AuthorId;
+	/**
+	 * this is the activation token for the Author
+	 * @var Uuid $AuthorActivationToken
+	 */
+	private $AuthorActivationToken;
+	/**
+	 * this is the Authors avatar url
+	 * @var AuthorAvatarUrl
+	 */
+	private $AuthorAvatarUrl;
+	/**
+	 * this is the Author email
+	 * @var AuthorEmail
+	 */
+	private $AuthorEmail;
+	/**
+	 * this is the Hash for the Author
+	 * @var $AuthorHash
+	 */
+	private $AuthorHash;
+	/**
+	 * this is the Authors username
+	 * @var $AuthorUsername
+	 */
+	private $AuthorUsername;
 
+	/**
+	 * constructor for this Author
+	 *
+	 * @param string|Uuid $newAuthorId id of this Author or null if a new Author
+	 * @param string|Uuid $newAuthorActivationToken activation token for new Author
+	 * @param string|Uuid $newAuthorAvatarUrl Avatar URL for the Author
+	 * @param string|Uuid $AuthorEmail
+	 */
 }
 ?>
